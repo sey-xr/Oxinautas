833b117b4c83ff11db583c6dd42c290be4c62203
Şey XR <seytantuyux@gmail.com>
2025-11-26 16:52:10 -0600

    Add player flip logic and update position in scene
    Introduced a Flip() method in PlayerController to handle player orientation based on movement direction. Also updated the player's position in the Oxinauta scene.

--- Unity/Oxinautas_v6/Assets/Scripts/PlayerController.cs
+++ Unity/Oxinautas_v6/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
     private GatherInput m_gatherInput;
     private Transform m_transform;
     [SerializeField] private float speed;
+    private int direction = 1;
     void Start()
     {
         m_gatherInput = GetComponent<GatherInput>();
@@ -14,7 +15,26 @@ public class PlayerController : MonoBehaviour
     }
     void FixedUpdate()
     {
+        Move();
+    }
+    private void Move()
+    {
+        // Método para comprobar si está girado el player
+        Flip();
         // Linear movement 30fps
         m_rigidbody2D.linearVelocity = new Vector2(speed * m_gatherInput.ValueX, m_rigidbody2D.linearVelocityY);
+    }
+    private void Flip()
+    {
+        if (m_gatherInput.ValueX * direction < 0)
+        {
+            m_transform.localScale = new Vector3(-m_transform.localScale.x, 1, 1);
+            direction *= -1;
+        }
+        else if (m_gatherInput.ValueX * direction > 0)
+        {
+            m_transform.localScale = new Vector3(m_transform.localScale.x, 1, 1);
+            direction *= 1;
+        }
     }
 }
